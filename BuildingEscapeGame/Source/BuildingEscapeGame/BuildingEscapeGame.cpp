92c4451a553b13905b990ff0e6d1c321dd96b51c
sks-shubham <shubhamkumarsonkar1999@gmail.com>
2019-02-05 22:38:18 +0530

    Initial Commit

---
+++ BuildingEscapeGame/Source/BuildingEscapeGame/BuildingEscapeGame.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BuildingEscapeGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BuildingEscapeGame, "BuildingEscapeGame" );
